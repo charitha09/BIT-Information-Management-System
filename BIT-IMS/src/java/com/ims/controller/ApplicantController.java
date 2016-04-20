699154e5555bc4e727d21d67c40588bafcbcdf1e
AmilaSankha <amilasankha@gmail.com>
2016-04-20 13:27:41 +0530

    Applicant model ,Student model and Applicant Controller

--- BIT-IMS/src/java/com/ims/controller/ApplicantController.java
+++ BIT-IMS/src/java/com/ims/controller/ApplicantController.java
@@ -5,10 +5,93 @@
  */
 package com.ims.controller;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.ims.model.Applicant;
+import com.ims.db.DBConnection;
+
 /**
  *
  * @author amila
  */
 public class ApplicantController {
-    
+
+    public static int saveApplicant(Applicant applicant) throws Exception {
+        int result = 0;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        String sql = "INSERT INTO applicant (application_num, full_name, initial, last_name, title, gender, citizenship, "
+                + "nationality, national_id, date_of_birth, address, country, telephone_num, email, al_subject_01, subject_01_result,"
+                + "al_subject_02, subject_02_result, al_subject_03, subject_03_result, ol_maths_result, 0l_english_result, is_employ, "
+                + "designation, is_computer_field, monthly_salary)"
+                + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+        try {
+            connection = new DBConnection().getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, applicant.getApplicationNum());
+            preparedStatement.setString(2, applicant.getFullName());
+            preparedStatement.setString(3, applicant.getInitial());
+            preparedStatement.setString(4, applicant.getLastName());
+            preparedStatement.setString(5, applicant.getTitle());
+            preparedStatement.setString(6, applicant.getGender());
+            preparedStatement.setString(7, applicant.getCitizenship());
+            preparedStatement.setString(8, applicant.getNationality());
+            preparedStatement.setString(9, applicant.getNicNo());
+            preparedStatement.setDate(10, applicant.getBirthDay());
+            preparedStatement.setString(11, applicant.getAddress());
+            preparedStatement.setString(12, applicant.getCountry());
+            preparedStatement.setString(13, applicant.getTelephoneNum());
+            preparedStatement.setString(14, applicant.getEmail());
+            preparedStatement.setString(15, applicant.getAlSubject01());
+            preparedStatement.setString(16, applicant.getSubject01Result());
+            preparedStatement.setString(17, applicant.getAlSubject02());
+            preparedStatement.setString(18, applicant.getSubject02Result());
+            preparedStatement.setString(19, applicant.getAlSubject03());
+            preparedStatement.setString(20, applicant.getSubject03Result());
+            preparedStatement.setString(21, applicant.getOlMathsResult());
+            preparedStatement.setString(22, applicant.getOlEnglishResult());
+            preparedStatement.setInt(23, applicant.getIsEmploy());
+            preparedStatement.setString(24, applicant.getDesignation());
+            preparedStatement.setInt(25, applicant.getIsComputerField());
+            preparedStatement.setInt(26, applicant.getMonthlySalary());
+            
+            result = preparedStatement.executeUpdate();
+            
+            
+            return result;
+        } catch (Exception ex) {
+            // operation failed
+            throw ex;
+        } finally{
+            if(preparedStatement != null){
+                preparedStatement.close();
+            }
+            if(connection != null){
+                connection.close();
+            }
+        }
+
+        
+    }
+ /*   
+    public static void main(String[] args) {
+        Date d = Date.valueOf(LocalDate.now());
+        System.out.println(d);
+        Applicant ap = new Applicant("a", "2", "4", "5", "6", "7", "8", "9", d, "1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", 4, "5", 6, 7);
+        ap.setFullName("amila");
+        int x = 20;
+        try {
+            x = ApplicantController.saveApplicant(ap);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        finally{
+            System.out.println(x);
+        }
+         }
+    */
 }
--- BIT-IMS/src/java/com/ims/model/Applicant.java
+++ BIT-IMS/src/java/com/ims/model/Applicant.java
@@ -5,10 +5,400 @@
  */
 package com.ims.model;
 
+import java.sql.Date;
+
 /**
  *
  * @author amila
  */
 public class Applicant {
+
+    private int applicationNum;
+    private String fullName;
+    private String initial;
+    private String lastName;
+    private String title;
+    private String gender;
+    private String citizenship;
+    private String nationality;
+    private String nicNo;
+    private Date birthDay;
+    private String address;
+    private String country;
+    private String telephoneNum;
+    private String email;
+    private String alSubject01;
+    private String Subject01Result;
+    private String alSubject02;
+    private String Subject02Result;
+    private String alSubject03;
+    private String Subject03Result;
+    private String olMathsResult;
+    private String olEnglishResult;
+    private int isEmploy;
+    private String designation;
+    private int isComputerField;
+    private int monthlySalary;
+
+    public Applicant() {
+    }
+
     
+    /**
+     * constructor without application number
+     * @param fullName
+     * @param initial
+     * @param lastName
+     * @param title
+     * @param gender
+     * @param citizenship
+     * @param nationality
+     * @param nicNo
+     * @param birthDay
+     * @param address
+     * @param country
+     * @param telephoneNum
+     * @param email
+     * @param alSubject01
+     * @param Subject01Result
+     * @param alSubject02
+     * @param Subject02Result
+     * @param alSubject03
+     * @param Subject03Result
+     * @param olMathsResult
+     * @param olEnglishResult
+     * @param isEmploy
+     * @param designation
+     * @param isComputerField
+     * @param monthlySalary 
+     */
+    public Applicant(String fullName, String initial, String lastName, String title, 
+                    String gender, String citizenship, String nationality, String nicNo, 
+                    Date birthDay, String address, String country, String telephoneNum, 
+                    String email, String alSubject01, String Subject01Result, String alSubject02, 
+                    String Subject02Result, String alSubject03, String Subject03Result, 
+                    String olMathsResult, String olEnglishResult, int isEmploy, String designation, 
+                    int isComputerField, int monthlySalary) {
+                this.fullName = fullName;
+        this.initial = initial;
+        this.lastName = lastName;
+        this.title = title;
+        this.gender = gender;
+        this.citizenship = citizenship;
+        this.nationality = nationality;
+        this.nicNo = nicNo;
+        this.birthDay = birthDay;
+        this.address = address;
+        this.country = country;
+        this.telephoneNum = telephoneNum;
+        this.email = email;
+        this.alSubject01 = alSubject01;
+        this.Subject01Result = Subject01Result;
+        this.alSubject02 = alSubject02;
+        this.Subject02Result = Subject02Result;
+        this.alSubject03 = alSubject03;
+        this.Subject03Result = Subject03Result;
+        this.olMathsResult = olMathsResult;
+        this.olEnglishResult = olEnglishResult;
+        this.isEmploy = isEmploy;
+        this.designation = designation;
+        this.isComputerField = isComputerField;
+        this.monthlySalary = monthlySalary;
+    }
+
+    
+    /**
+     * 
+     * @param applicationNum
+     * @param fullName
+     * @param initial
+     * @param lastName
+     * @param title
+     * @param gender
+     * @param citizenship
+     * @param nationality
+     * @param nicNo
+     * @param birthDay
+     * @param address
+     * @param country
+     * @param telephoneNum
+     * @param email
+     * @param alSubject01
+     * @param Subject01Result
+     * @param alSubject02
+     * @param Subject02Result
+     * @param alSubject03
+     * @param Subject03Result
+     * @param olMathsResult
+     * @param olEnglishResult
+     * @param isEmploy
+     * @param designation
+     * @param isComputerField
+     * @param monthlySalary 
+     */
+    public Applicant(int applicationNum, String fullName, String initial,
+                    String lastName, String title, String gender, String citizenship,
+                    String nationality, String nicNo, Date birthDay, String address,
+                    String country, String telephoneNum, String email, String alSubject01,
+                    String Subject01Result, String alSubject02, String Subject02Result,
+                    String alSubject03, String Subject03Result, String olMathsResult,
+                    String olEnglishResult, int isEmploy, String designation,
+                    int isComputerField, int monthlySalary) {
+        super();
+        this.applicationNum = applicationNum;
+        this.fullName = fullName;
+        this.initial = initial;
+        this.lastName = lastName;
+        this.title = title;
+        this.gender = gender;
+        this.citizenship = citizenship;
+        this.nationality = nationality;
+        this.nicNo = nicNo;
+        this.birthDay = birthDay;
+        this.address = address;
+        this.country = country;
+        this.telephoneNum = telephoneNum;
+        this.email = email;
+        this.alSubject01 = alSubject01;
+        this.Subject01Result = Subject01Result;
+        this.alSubject02 = alSubject02;
+        this.Subject02Result = Subject02Result;
+        this.alSubject03 = alSubject03;
+        this.Subject03Result = Subject03Result;
+        this.olMathsResult = olMathsResult;
+        this.olEnglishResult = olEnglishResult;
+        this.isEmploy = isEmploy;
+        this.designation = designation;
+        this.isComputerField = isComputerField;
+        this.monthlySalary = monthlySalary;
+    }
+
+    public int getApplicationNum() {
+        return applicationNum;
+    }
+
+    public void setApplicationNum(int applicationNum) {
+        this.applicationNum = applicationNum;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getInitial() {
+        return initial;
+    }
+
+    public void setInitial(String initial) {
+        this.initial = initial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCitizenship() {
+        return citizenship;
+    }
+
+    public void setCitizenship(String citizenship) {
+        this.citizenship = citizenship;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getNicNo() {
+        return nicNo;
+    }
+
+    public void setNicNo(String nicNo) {
+        this.nicNo = nicNo;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTelephoneNum() {
+        return telephoneNum;
+    }
+
+    public void setTelephoneNum(String telephoneNum) {
+        this.telephoneNum = telephoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAlSubject01() {
+        return alSubject01;
+    }
+
+    public void setAlSubject01(String alSubject01) {
+        this.alSubject01 = alSubject01;
+    }
+
+    public String getSubject01Result() {
+        return Subject01Result;
+    }
+
+    public void setSubject01Result(String Subject01Result) {
+        this.Subject01Result = Subject01Result;
+    }
+
+    public String getAlSubject02() {
+        return alSubject02;
+    }
+
+    public void setAlSubject02(String alSubject02) {
+        this.alSubject02 = alSubject02;
+    }
+
+    public String getSubject02Result() {
+        return Subject02Result;
+    }
+
+    public void setSubject02Result(String Subject02Result) {
+        this.Subject02Result = Subject02Result;
+    }
+
+    public String getAlSubject03() {
+        return alSubject03;
+    }
+
+    public void setAlSubject03(String alSubject03) {
+        this.alSubject03 = alSubject03;
+    }
+
+    public String getSubject03Result() {
+        return Subject03Result;
+    }
+
+    public void setSubject03Result(String Subject03Result) {
+        this.Subject03Result = Subject03Result;
+    }
+
+    public String getOlMathsResult() {
+        return olMathsResult;
+    }
+
+    public void setOlMathsResult(String olMathsResult) {
+        this.olMathsResult = olMathsResult;
+    }
+
+    public String getOlEnglishResult() {
+        return olEnglishResult;
+    }
+
+    public void setOlEnglishResult(String olEnglishResult) {
+        this.olEnglishResult = olEnglishResult;
+    }
+
+    public int getIsEmploy() {
+        return isEmploy;
+    }
+
+    public void setIsEmploy(int isEmploy) {
+        this.isEmploy = isEmploy;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getIsComputerField() {
+        return isComputerField;
+    }
+
+    public void setIsComputerField(int isComputerField) {
+        this.isComputerField = isComputerField;
+    }
+
+    public int getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(int monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Applicant{" + "applicationNum=" + applicationNum + ", fullName=" 
+                + fullName + ", initial=" + initial + ", lastName=" + lastName 
+                + ", title=" + title + ", gender=" + gender + ", citizenship=" 
+                + citizenship + ", nationality=" + nationality + ", nicNo=" + nicNo 
+                + ", birthDay=" + birthDay + ", address=" + address + ", country=" 
+                + country + ", telephoneNum=" + telephoneNum + ", email=" + email 
+                + ", alSubject01=" + alSubject01 + ", Subject01Result=" + Subject01Result 
+                + ", alSubject02=" + alSubject02 + ", Subject02Result=" + Subject02Result 
+                + ", alSubject03=" + alSubject03 + ", Subject03Result=" + Subject03Result 
+                + ", olMathsResult=" + olMathsResult + ", olEnglishResult=" + olEnglishResult 
+                + ", isEmploy=" + isEmploy + ", designation=" + designation + ", isComputerField=" 
+                + isComputerField + ", monthlySalary=" + monthlySalary + '}';
+    }
+    
+    
+    
+    
+
 }
