fa29d0d08cfba47676de4452859f424b3bea6ee8
AmilaSankha <amilasankha@gmail.com>
2016-05-09 23:54:40 +0530

    Generate Application number

---
+++ BIT-IMS/src/java/com/ims/ids/ApplicationNum.java
@@ -0,0 +1,36 @@
+package com.ims.ids;
+
+import java.io.Serializable;
+import java.sql.*;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+public class ApplicationNum implements IdentifierGenerator {
+
+    @Override
+    public Serializable generate(SessionImplementor session, Object object)
+            throws HibernateException {
+
+        String prefix = "2016_A_";
+        Connection connection = session.connection();
+
+        try {
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("select count(applicationNum) as applicationNum from applicant");
+
+            if (rs.next()) {
+                int id = rs.getInt(1) + 1;
+                String generatedId = prefix + new Integer(id).toString();
+                System.out.println("Generated Id: " + generatedId);
+                return generatedId;
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
