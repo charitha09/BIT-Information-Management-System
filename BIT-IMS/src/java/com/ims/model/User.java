225b172dd21b6b19c7cc60064865de404969e22a
AmilaSankha <amilasankha@gmail.com>
2016-05-18 13:07:42 +0530

    replace user model

--- BIT-IMS/src/java/com/ims/model/User.java
+++ BIT-IMS/src/java/com/ims/model/User.java
@@ -1,91 +1,99 @@
-
-package com.ims.model;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-/**
- *
- * @author Indusaranie Tharusha
- */
-public class User {
-    private int userID;
-    private String userName;
-    private String email;
-    private String password;
-    private String userLevel;
-    
-    public User(){
-    }
-    
-    public User(int userID, String userName, String email, String password, String userLevel){
-        this.userID = userID;
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-        this.userLevel = userLevel;
-    }
-    
-    public int getUserID() {
-        return userID;
-    }
-
-    public void setUserID(int userID) {
-        this.userID = userID;
-    }
-    
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-     public String getUserLevel() {
-        return userLevel;
-    }
-
-    public void setUserLevel(String userLevel) {
-        this.userLevel = userLevel;
-    }
-    
-    @Override
-    public String toString(){
-        return "User{" + "userID=" + userID + ", userName=" + userName + ", email=" + email 
-                + ", password=" + password + ", userLevel=" + userLevel + '}';
-                
-    }
-    
-    public static void main(String[] args) {
-        User use = new User();
-        use.setUserName("sdwheq");
-        
-        SessionFactory sf = new Configuration().configure().buildSessionFactory();
-        Session s = sf.openSession();
-        s.beginTransaction();
-        
-        s.save(use);
-        
-        s.getTransaction().commit();
-        
-    }
-
-}
+   package com.ims.model;
+   
+   import javax.persistence.Entity;
+   import javax.persistence.GeneratedValue;
+   import javax.persistence.Id;
+   import javax.persistence.Table;
+   import javax.persistence.UniqueConstraint;
+   import org.hibernate.Session;
+   import org.hibernate.SessionFactory;
+   import org.hibernate.annotations.GenericGenerator;
+   import org.hibernate.cfg.Configuration;
+   
+   /**
+    *
+    * @author amila
+    */
+   @Entity
+   @Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+   public class User {
+   
+       @Id
+       @GenericGenerator(name = "sequence_user_id", strategy = "com.ims.ids.UserId")
+       @GeneratedValue(generator = "sequence_user_id")
+       private String userId;
+       private String name;
+       private String email;
+       private String userLevel;
+       private String userName;
+       private String password;
+   
+       public User() {
+       }
+   /**
+    * 
+    * @param userId
+    * @param name
+    * @param email
+    * @param userLevel
+    * @param userName
+    * @param password 
+    */
+       public User(String userId, String name, String email, String userLevel, String userName, String password) {
+           this.userId = userId;
+           this.name = name;
+           this.email = email;
+           this.userLevel = userLevel;
+           this.userName = userName;
+           this.password = password;
+       }
+   
+       public String getUserId() {
+           return userId;
+       }
+   
+       public void setUserId(String userId) {
+           this.userId = userId;
+       }
+   
+       public String getName() {
+           return name;
+       }
+   
+       public void setName(String name) {
+           this.name = name;
+       }
+   
+       public String getEmail() {
+           return email;
+       }
+   
+       public void setEmail(String email) {
+           this.email = email;
+       }
+   
+       public String getUserLevel() {
+           return userLevel;
+       }
+   
+       public void setUserLevel(String userLevel) {
+           this.userLevel = userLevel;
+       }
+   
+       public String getUserName() {
+           return userName;
+       }
+   
+       public void setUserName(String userName) {
+           this.userName = userName;
+       }
+   
+       public String getPassword() {
+           return password;
+       }
+   
+       public void setPassword(String password) {
+           this.password = password;
+       }
+       
+   }    
